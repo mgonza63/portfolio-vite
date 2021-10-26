f4d6142f4a5087ced31c844e625ecf025c3eff92
Mauricio Gonzalez <mauglezg21@gmail.com>
2021-10-26 12:22:54 -0500

    added projects section

--- main.js
+++ main.js
@@ -3,3 +3,5 @@ import {gsap} from 'gsap';
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.circleText', {rotation:360, duration: 45, repeat: -1})
